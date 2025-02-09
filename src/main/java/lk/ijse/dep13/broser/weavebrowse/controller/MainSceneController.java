c89c7cc72049a7d50b7763d6a74c39f8ac69bfbc
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-02-09 13:48:00 +0530

    Starting the MainSceneController

--- src/main/java/lk/ijse/dep13/broser/weavebrowse/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/broser/weavebrowse/controller/MainSceneController.java
@@ -1,4 +1,17 @@
 package lk.ijse.dep13.broser.weavebrowse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
+
 public class MainSceneController {
+    public AnchorPane root;
+    public TextField txtAddress;
+    public WebView wbDisplay;
+
+    public void txtAddressOnAction(ActionEvent event) {
+
+    }
+
 }
